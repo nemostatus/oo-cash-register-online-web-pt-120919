863ee1fed0a802fe76a29e326b471050c7513873
Matthew Corralejo <mattcorralejo1@gmail.com>
2020-01-16 13:54:17 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,20 @@
+ def add_item(name,price,number=1)
+   if number>1 
+     i = 0
+     while i<number
+     @items << name 
+     i+=1 
+   end 
+ else 
+   @items << name 
+ end 
+ @total+=price*number
+ @last_transaction_amount = @total 
+ @total
+ end 
+ 
+
+
+
+
 
